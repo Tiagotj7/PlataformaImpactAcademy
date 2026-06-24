29de018a2a33a865957ab4b5af9ac1b701245142
Tiagotj7 <tiagowork07@gmail.com>
2026-06-24 01:27:11 -0300

    updated add favicon

--- app/views/layout.php
+++ app/views/layout.php
@@ -7,6 +7,7 @@ use App\Core\Auth;
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= e(($title ?? 'Impact Academy')) ?></title>
+  <link rel="icon" href="<?= url('assets/images/favicon.ico') ?>" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="<?= url('assets/css/style.css') ?>">
 </head>
